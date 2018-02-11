f4a7650663b91499ab484f4f8ea097d1575088a3
itggot-adin-smailhodzic <adin.smailhodzic@itggot.se>
2018-02-11 22:13:00 +0100

    Dag 6
    Sista tillfället innan inlämning. Projektet är för det mesta klart, fast bra CSS och ett fåtal buggar finns kvar.

--- app.rb
+++ app.rb
@@ -60,16 +60,25 @@ class App < Sinatra::Base
 
 	post '/accept_friend' do
 
-		friend_id = params[:user_id]
+		id = params[:table_id].to_i
 
 		db = SQLite3::Database.new('database.db')
-		db.execute("UPDATE Relations SET Relation_State = 1 WHERE User_1 OR User_2 = ?", [friend_id])
+		db.execute("UPDATE Relations SET Relation_State = 1 WHERE ID = #{id}")
 
 		redirect('/profile/' + session[:id].to_s)
 
+	end
+	post '/decline_friend' do
+
+		id = params[:table_id].to_i
+
+		db = SQLite3::Database.new('database.db')
+		db.execute("DELETE FROM Relations WHERE ID = #{id}")
+
+		redirect('/profile/' + session[:id].to_s)
 
 	end
-
+ 
 	get '/profile/:id' do
 
 		id = params[:id].to_i
@@ -78,31 +87,27 @@ class App < Sinatra::Base
 			redirect('/error')
 		end
 
-		if(session[:id])
-			db = SQLite3::Database.new('database.db')
-			db.results_as_hash = true
+		session[:id] = id
 
-			result = db.execute("SELECT User_1, User_2, Relation_State, User_Action FROM Relations WHERE User_1 OR User_2 = ?", [session[:id].to_i])
+		db = SQLite3::Database.new('database.db')
 
-			result_string = result.to_s
+		result = db.execute("SELECT * FROM Relations WHERE (User_1 = #{session[:id]} OR User_2 = #{session[:id]})")
 
-			index = 0
-			values = []
+		# result_string = result.to_s
+
+		# index = 0
+		# values = []
 		  
-			result_string.each_char do |i|
-			  if i == '>'[0] then
-				values << index + 1
-			  end
-			  index += 1
-			end
+		# result_string.each_char do |i|
+		#   if i == '>'[0] then
+		# 	values << index + 1
+		#   end
+		#   index += 1
+		# end
 
-			var = 0 
+		var = 0 
 		  
-			slim(:menu, locals:{notes:result, values:values, id:id, i:var})
-		else
-			redirect('/')
-		end
-
+		slim(:menu, locals:{notes:result, id:id, i:var})
 	end
 
 	post '/friend_request' do
@@ -125,7 +130,7 @@ class App < Sinatra::Base
 		
 		user_reciever_id_instance = db.execute("SELECT * FROM Users WHERE id = ?", [User_reciever])
 
-		if user_reciever_id_instance.empty? == false
+		if user_reciever_id_instance.empty? == false # Checks if the user recipent is non-existant
 
 			if User_sender < User_reciever
 				db.execute("INSERT INTO Relations(User_1,User_2,Relation_State,User_Action) VALUES(?,?,0,?)", [User_sender, User_reciever, User_sender])
@@ -142,8 +147,6 @@ class App < Sinatra::Base
 			redirect('/error')
 
 		end
-
-	
 
 	end
 
